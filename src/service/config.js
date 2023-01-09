cc3d8111c000fc1e4239b6e8f3d224507b217e53
dhir0hit <rohit786dhir@gmail.cp>
2023-01-09 14:24:52 -0500

    config file for api

---
+++ src/service/config.js
@@ -0,0 +1,58 @@
+const ServerConfig =
+    {
+        api: {
+            protocol: "http", // http or https
+            hostname: "10.0.0.112", // xxx.xxx.xxx.xxx or www.example.domain.com
+            port    : "5000", // 80 || 5000
+            index: {
+                name: '',
+                checkConnection: 'check-connection',
+            },
+            passwordManager: {
+                name    : "pwdmanager", // Controller for password manager
+                routes: {
+                    getAll: "get-all", // Get All Accounts Page
+                    getOne: "account", // Get One Accounts Page
+                    create: "create",  // Create  Account  Page
+                    update: "update",  // Update  Account  Page
+                    delete: "delete"   // Delete  Account  Page
+                }
+            }
+        }
+    }
+
+export default class Config {
+    /**
+     * Creating Link for index controller api
+     * */
+    static CheckConnection = `${ServerConfig.api.protocol}://${ServerConfig.api.hostname}:${ServerConfig.api.port}/${ServerConfig.api.index.checkConnection}/`;
+
+
+    /**
+     *  Creating Link for Password manager controller api
+     *  */
+    static pwdManagerBackendLink = `${ServerConfig.api.protocol}://${ServerConfig.api.hostname}:${ServerConfig.api.port}/${ServerConfig.api.passwordManager.name}/`;
+    /*
+    * Creating Links to other pages of api
+    * */
+    static GetAllAccountsLink() {
+        // GET ALL ACCOUNTS
+        return this.pwdManagerBackendLink + ServerConfig.api.passwordManager.routes.getAll + '/';
+    }
+    static GetOneAccountLink() {
+        // GET ONE ACCOUNT
+        return this.pwdManagerBackendLink + ServerConfig.api.passwordManager.routes.getOne + '/';
+    }
+    static CreateAccountLink() {
+        // CREATE ACCOUNT
+        return this.pwdManagerBackendLink + ServerConfig.api.passwordManager.routes.create + '/';
+    }
+    static UpdateAccountLink() {
+        // UPDATE ACCOUNT
+        return this.pwdManagerBackendLink + ServerConfig.api.passwordManager.routes.update + '/';
+    }
+    static DeleteAccountLink() {
+        // DELETE ACCOUNT
+        return this.pwdManagerBackendLink + ServerConfig.api.passwordManager.routes.delete + '/';
+    }
+}
